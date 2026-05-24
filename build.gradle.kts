4ab92e084a57803e1748c3ae6ae8953b66fd1817
JesusValera <jesus1902@outlook.com>
2026-05-25 00:05:40 +0200

    feat: bump to v0.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.grammarkit.tasks.GenerateLexerTask
 import org.jetbrains.grammarkit.tasks.GenerateParserTask
 
 group = "org.phellang"
-version = "0.4.2"
+version = "0.4.3"
 
 plugins {
     id("java")
