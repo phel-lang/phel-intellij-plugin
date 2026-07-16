778f650c6b3598f6b0468f28207a0086c58cbb70
JesusValera <jesus1902@outlook.com>
2026-07-16 11:47:30 +0200

    ref: convert PhelTokenSets constant interface to object

--- src/main/kotlin/org/phellang/language/psi/elements/PhelTokenSets.kt
+++ src/main/kotlin/org/phellang/language/psi/elements/PhelTokenSets.kt
@@ -3,9 +3,7 @@ package org.phellang.language.psi.elements
 import com.intellij.psi.tree.TokenSet
 import org.phellang.language.psi.PhelTypes
 
-interface PhelTokenSets {
-    companion object {
-        val LINE_COMMENT: TokenSet = TokenSet.create(PhelTypes.LINE_COMMENT, PhelTypes.FORM_COMMENT)
-        val SYM: TokenSet = TokenSet.create(PhelTypes.SYM)
-    }
+object PhelTokenSets {
+    val LINE_COMMENT: TokenSet = TokenSet.create(PhelTypes.LINE_COMMENT, PhelTypes.FORM_COMMENT)
+    val SYM: TokenSet = TokenSet.create(PhelTypes.SYM)
 }
